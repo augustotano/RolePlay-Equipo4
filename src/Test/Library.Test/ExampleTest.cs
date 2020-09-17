fc84b226ec80debdbaf15bb4780f40add7fc727c
Augusto Taño <augusto.1812@hotmail.com>
2020-09-17 18:52:39 -0300

    Update ExampleTest.cs

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -1,16 +1,29 @@
 using NUnit.Framework;
+using Library;
 
 namespace Test.Library
 {
 
 
-    public class ExampleTest
+    public class CharacterTests
     {
 
         [Test]
-        public void dummyTest()
+        public void AddSpellTestWizard()
         {
-            Assert.True(true);
+            Character P1 = new Character("Raziel", "WiZaRD", 0, 20);
+            Spell fireball = new Spell("Fireball", 40, 0, "Lanza una bola de fuego");
+            P1.AddSpell(fireball);
+            Assert.IsNotEmpty(P1.bookOfSpells);
+        }
+
+        [Test]
+        public void AddSpellTestNonWizard()
+        {
+            Character P1 = new Character("Edmond", "Caballero", 40, 40);
+            Spell fireball = new Spell("Fireball", 40, 0, "Lanza una bola de fuego");
+            P1.AddSpell(fireball);
+            Assert.IsEmpty(P1.bookOfSpells);
         }
 
     }
