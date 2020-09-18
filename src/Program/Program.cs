f5c2fc99680ef3fb36f11b1c6151d59b50e82a1d
Augusto Taño <augusto.1812@hotmail.com>
2020-09-17 21:32:21 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -13,11 +13,17 @@ namespace Program
             Gandalf.AddSpell(Freeze);
             Gandalf.AttachObject(MagicScepter);
 
-            Character Arthur = new Character("Arthur","King",30,20);
-            Element Sword = new Element("Magic Sword",60,40);
-            Element Shield = new Element("The Protector",50,60);
+            Character Arthur = new Character("Arthur","King", 30, 20);
+            Element Sword = new Element("Magic Sword", 60, 40);
+            Element Shield = new Element("The Protector", 50, 60);
             Arthur.AttachObject(Sword);
             Arthur.AttachObject(Shield);
+
+            Character Loki = new Character("Loki","Assassin", 50, 0);
+            Element Dagger = new Element("Night's Fall", 80, 0);
+            Element Cloak = new Element("Faceless' Cloak", 0, 30);
+            Loki.AttachObject(Dagger);
+            Loki.AttachObject(Cloak);
         }
     }
 }
