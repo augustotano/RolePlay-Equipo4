0c167710a680d910ca00ab05cd5cd326491cace1
ValentinaAlvarezIgarzabal <61558623+ValentinaAlvarezIgarzabal@users.noreply.github.com>
2020-09-18 14:52:26 -0300

    Comentarios de la logica del juego

--- src/Library/Element.cs
+++ src/Library/Element.cs
@@ -2,6 +2,19 @@ using System;
 
 namespace Library
 {
+    /*
+        SINGLE RESPONSABILITY PRINCIPLE - SRP
+        Se establece que cada clase debe tener responsabilidad sobre una parte de la funcionalidad
+        proporcionada por el software. Los metodos y atributos de la clase deben estar 
+        alineados con esa responsabillidad. 
+
+        "Una clase debe tener solo una razon para cambiar"
+
+        En el caso de querer cambiar como se representan los elementos del juego, la clase 
+        Element es quien se debe modifiicar. Siendo esta la unica razon de cambio.
+
+
+    */
     public class Element
     {
         public string Name {set; get;}
